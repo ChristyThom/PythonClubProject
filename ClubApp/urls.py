91f477ec443c0d578f9ecaa3d78165c180c2b582
Christy Thom <christyraethom@gmail.com>
2019-05-02 13:53:13 -0700

    Upload for Assignment Six

--- ClubApp/urls.py
+++ ClubApp/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns=[
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('getResources/', views.getResources, name='resources'),
+    path('getMeeting/', views.getMeeting, name='meeting'),
+    path('meetingDetails/<int:id>', views.meetingDetails, name='meetingdetails'),
 ]
--- ClubApp/views.py
+++ ClubApp/views.py
@@ -1,7 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Meeting, MeetingMinutes, Resource, Events
 
 # Create your views here.
 
 def index(request):
     return render(request, 'ClubApp/index.html')
+
+def getResources(request):
+    resources_list=Resource.objects.all()
+    context={'resources_list' : resources_list}
+    return render(request, 'ClubApp/resources.html' , context=context)
+
+def getMeeting(request):
+    meeting_list=Meeting.objects.all()
+    return render(request, 'ClubApp/meeting.html', {'meeting_list' : meeting_list})
+
+def meetingDetails(request, id):
+    meeting=get_object_or_404(Meeting, pk=id)
+    context={
+        'meeting' : meeting,
+    }
+    return render (request, 'ClubApp/meetingdetails.html', context=context)
+
+
     
